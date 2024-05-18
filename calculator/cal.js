3d6039d6dc90e4dad7760012d25f9db9b69533f5
Trailblazer-dev <richvictor830@gmail.com>
2024-05-18 21:19:25 +0300

    update

--- calculator/cal.js
+++ calculator/cal.js
@@ -1,20 +1,28 @@
 function appendToResult(value) {
-  document.getElementById('result').value += value;
+  const resultInput = document.getElementById("result");
+  if (value === "÷") {
+    resultInput.value += "/";
+  } else if (value === "×") {
+    resultInput.value += "*";
+  } else {
+    resultInput.value += value;
+  }
 }
 
 function clearResult() {
-  document.getElementById('result').value = '';
+  document.getElementById("result").value = "";
 }
 
 function calculateResult() {
   try {
-    const result = eval(document.getElementById('result').value);
-    document.getElementById('result').value = result;
+    const result = eval(document.getElementById("result").value);
+    document.getElementById("result").value = result;
   } catch (error) {
-    document.getElementById('result').value = 'Error';
+    document.getElementById("result").value = "Error";
   }
 }
+
 function backspace() {
-  const result = document.getElementById('result').value;
-  document.getElementById('result').value = result.slice(0, -1);
+  const result = document.getElementById("result").value;
+  document.getElementById("result").value = result.slice(0, -1);
 }
