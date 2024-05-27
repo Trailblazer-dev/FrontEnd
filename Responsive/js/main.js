e68b74189b1086f3526fd2b65320e7054c787f7c
Trailblazer-dev <richvictor830@gmail.com>
2024-05-27 18:30:10 +0300

    update

---
+++ Responsive/js/main.js
@@ -0,0 +1,45 @@
+/*====SHOW MENU =====*/
+const navMenu=document.getElementById('nav-menu'),
+navToggle= document.getElementById('nav-toggle'),
+navClose= document.getElementById('nav-close');
+
+/*===== MENU SHOW =====*/
+/*Validate if const exitst */
+if(navToggle){
+    navToggle.addEventListener(`click`,() => {
+        navMenu.classList.add(`show-menu`)
+    })
+}
+/*===== MENU HIDDEN =====*/
+if(navClose){
+    navClose.addEventListener(`click`,() => {
+        navMenu.classList.remove(`show-menu`)
+    })
+}
+/*========= Remove Menu =================*/
+const navLink=document.querySelectorAll('.nav__link');
+
+function linkAction(){
+    const  navMenu =document.querySelectorAll('nav-menu');
+    // When  we click on each nav__link ,we remove the show-menu class
+    navMenu.classList.remove(`show-menu`);
+}
+navLink.forEach(n => n.addEventListener('click',linkAction));
+
+/*============= Change background header ============ */
+function scrollHeader(){
+    const nav=document.getElementById('header');
+    // When the scroll is greater than 50 viewport height,add the scroll-header class to the header tag
+    if(this.scrollY >= 50){
+        nav.classList.add('scroll-header');
+    }else{
+        nav.classList.remove('scroll-header');
+    }
+}
+window.addEventListener('scroll',scrollHeader)
+
+/*==============Testimonial swiper============= */
+/*let TestimonialSwiper = new Swiper(".testimonial-swiper"){
+    spaceBetween:30,
+    loop:'true'
+}*
