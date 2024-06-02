be275045a8fa86cbb7ce392130c832dec2458d91
Trailblazer-dev <richvictor830@gmail.com>
2024-06-02 20:01:20 +0300

    update

--- Responsive/js/main.js
+++ Responsive/js/main.js
@@ -27,25 +27,24 @@ navClose= document.getElementById('nav-close');
 navLink.forEach(n => n.addEventListener('click',linkAction));
 
 /*============= Change background header ============ */
-function scrollHeader(){
-    const nav=document.getElementById('header');
-    // When the scroll is greater than 50 viewport height,add the scroll-header class to the header tag
-    if(this.scrollY >= 50){
-        nav.classList.add('scroll-header');
-    }else{
-        nav.classList.remove('scroll-header');
-    }
+function scrollHeader() {
+  const nav = document.getElementById("header");
+  // When the scroll is greater than 50 viewport height,add the scroll-header class to the header tag
+  if (this.scrollY >= 50) {
+    nav.classList.add("scroll-header");
+  } else {
+    nav.classList.remove("scroll-header");
+  }
 }
-window.addEventListener('scroll',scrollHeader)
-
+window.addEventListener("scroll", scrollHeader);
 
 /*=============== SWIPER POPULAR ===============*/
 var swiperPopular = new Swiper(".popular__container", {
-    spaceBetween:32,
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView:'auto',
-    loop:true,
+  spaceBetween: 32,
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  loop: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
@@ -53,15 +52,34 @@ var swiperPopular = new Swiper(".popular__container", {
 });
 
 /*=============== VALUE ACCORDION ===============*/
+const accordionItems = document.querySelectorAll(".value__accordion-item");
+accordionItems.forEach((item) => {
+  const accordionHeader = item.querySelector(".value__accordion-header");
+  accordionHeader.addEventListener("click", () => {
+    const openItem =  document.querySelector('.accordion-open');
 
+    toggleItem(item);
+    if(openItem&& openItem!==item){
+        toggleItem(openItem);
+    }
+  });
+});
 
+const toggleItem = (item) => {
+  const accordionContent = item.querySelector(".value__accordion-content");
+  if (item.classList.contains("accordion-open")) {
+    accordionContent.removeAttribute("style");
+    item.classList.remove("accordion-open");
+  } else {
+    accordionContent.style.height = accordionContent.scrollHeight + "px";
+    item.classList.add("accordion-open");
+  }
+
+};
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
+/*=============== SHOW SCROLL UP ===============*/
 
-/*=============== SHOW SCROLL UP ===============*/ 
-
-
-/*=============== DARK LIGHT THEME ===============*/ 
-
+/*=============== DARK LIGHT THEME ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
