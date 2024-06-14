cd3fb03dbab7bd05683b76532869aa96753623e3
Trailblazer-dev <richvictor830@gmail.com>
2024-06-14 23:01:18 +0300

    Finished Responsive Real Estate website

--- Responsive Real Estate website/js/main.js
+++ Responsive Real Estate website/js/main.js
@@ -126,6 +126,7 @@ if(selectedTheme){
 
 
 }
+
 //activate  /deactivate theme  manually  with the button
 themeButton.addEventListener('click',()=>{
   //Add or remove the dark / icon theme
@@ -137,3 +138,18 @@ themeButton.addEventListener('click',()=>{
 });
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr= ScrollReveal({
+  origin:'top',
+  distance:'60px',
+  duration:'2500',
+  delay:400,
+  //reset:true
+});
+sr.reveal(`.home__title, .popular__container, .subscribe__container, .footer__container`);
+sr.reveal('.home__description',{delay:500});
+sr.reveal('.home__search',{delay:600});
+sr.reveal('.home__value',{delay:700});
+sr.reveal('.home__images',{delay:800,origin:'bottom'});
+sr.reveal('.logos__img',{interval:100})
+sr.reveal(`.value__images, .contact__content`,{origin:'left'})
+sr.reveal(`.value__content, .contact__images`,{origin:'right'})
